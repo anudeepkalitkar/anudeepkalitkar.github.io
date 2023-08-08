8b9be8dd22579d93edea7e9087f700dd22299ade
Anudeep Kalitkar <50821904+anudeepkalitkar@users.noreply.github.com>
2023-08-08 10:35:00 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+https://benrogers.dev/
+https://brittanychiang.com/
+https://vanholtz.co/
+https://www.adhamdannaway.com/
+https://www.rammaheshwari.com/
+https://materializecss.com/
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css'
-import 'materialize-css';
+import M from "materialize-css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
+  document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+  })
 );
 
 // If you want to start measuring performance in your app, pass a function
