81d79f9adb13aa4406989dd1d126ce621117523d
Anudeep Kalitkar <50821904+anudeepkalitkar@users.noreply.github.com>
2024-03-02 13:11:16 -0700

    SBAS

--- src/StaticInformation/ProjectInfo.js
+++ src/StaticInformation/ProjectInfo.js
@@ -193,9 +193,9 @@ const ProjectsInfo = [
 	},
 
 	{
-		title: "Image Similarity Based Authentication System",
-		coverImage: "Images/IBAS-IC.jpg",
-		intro: `Navigating a transformative shift in the world of digital authentication, the Image Similarity Based Authentication System presents an innovative paradigm. This system permits users to authenticate themselves by drawing an image, which is then cross-verified with their registration sketch, completely bypassing the need for traditional text-based passwords. Marrying the strengths of visual memory with modern tech, the system leverages HTML, CSS, and JS for its frontend, while Python, augmented with OpenCV, powers the backend.`,
+		title: "Signature Based Authentication System",
+		coverImage: "Images/SBAS.webp",
+		intro: `Navigating a transformative shift in the world of digital authentication, the Signature Based Authentication System presents an innovative paradigm. This system permits users to authenticate themselves by drawing an image, which is then cross-verified with their registration sketch, completely bypassing the need for traditional text-based passwords. Marrying the strengths of visual memory with modern tech, the system leverages HTML, CSS, and JS for its frontend, while Python, augmented with OpenCV, powers the backend.`,
 		description: `Existing System:
 		Predominantly, the digital authentication world relies heavily on the conventional duo of email and password. While some avant-garde platforms have dabbled with biometrics and smart cards, they come with their own set of challenges:
 
@@ -203,7 +203,7 @@ const ProjectsInfo = [
 
 		Everyday Usability: Although technologically superior, the day-to-day practicality and accessibility for general users of such advanced systems remain questionable.
 
-		Proposed Image Similarity Based Authentication System:
+		Proposed Signature Based Authentication System:
 		Stepping away from convention, this system introduces a fresh, user-centric approach to digital security:
 
 		Canvas for User’s Unique Mark:
@@ -221,9 +221,9 @@ const ProjectsInfo = [
 		Tech Stack:
 
 		The frontend, which presents the canvas and handles user interaction, is constructed using HTML, CSS, and JavaScript. In contrast, the backend, responsible for image processing, comparison, and storage, is efficiently managed by Python coupled with the OpenCV library.
-		To encapsulate, the Image Similarity Based Authentication System is a vanguard in digital access, blending the innate human proclivity for visuals with cutting-edge technology. As the digital landscape continues to expand, such intuitive and personalized systems herald a future where security is both stringent and user-friendly.`,
+		To encapsulate, the Signature Based Authentication System is a vanguard in digital access, blending the innate human proclivity for visuals with cutting-edge technology. As the digital landscape continues to expand, such intuitive and personalized systems herald a future where security is both stringent and user-friendly.`,
 		link: "https://anudeepkalitkar.github.io/Image-Based-Authentication-System-Signature/",
-		urlname: "IBASSimilarity",
+		urlname: "SBAS",
 		tools: ["Python", "HTML", "CSS", "JS", "Php"],
 		sourceCode:
 			"https://github.com/anudeepkalitkar/Image-Based-Authentication-System-Signature",
